e0c74d0ed5a5fa4ef3dc330e2dcf587435cf5ba3
marwin.philips@gmail.com <marwin.philips@gmail.com@ef681cb1-40c9-8e58-19f0-1f2f723a8d04>
2014-12-04 13:29:05 +0000

    javaFX Exercises

---
+++ src/javaFX/Timer.java
@@ -0,0 +1,65 @@
+package javaFX;
+
+import javafx.beans.property.StringProperty;
+
+public class Timer implements Runnable {
+	private int millisToWait;
+	private int ticks = 0;
+	private Thread t;
+	private StopWatch gui;
+	
+	public Timer(int millisToWait, StopWatch gui) {
+		this.millisToWait = millisToWait;
+		this.gui = gui;
+	}
+
+	@Override
+	public void run() {
+		while (t != null) {
+			
+			updateGui();
+			ticks++;
+			try {
+				Thread.sleep(millisToWait);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public void start() {
+		if (t == null) {
+			t = new Thread(this);
+			t.setDaemon(true);
+			t.start();
+		}
+	}
+
+	public void stop() {
+		t = null;
+		updateGui();
+	}
+
+	public void reset() {
+		ticks = 0;
+		updateGui();
+	}
+
+	public void updateGui() {		
+		gui.update(getTimeString());
+	}
+
+	private int getTimeInMillis() {
+		return ticks * millisToWait;
+	}
+
+	private String getTimeString() {
+		int timeInMillis = getTimeInMillis();
+		return (timeInMillis / 1000) + ":" + (timeInMillis % 1000);
+	}
+	
+	public boolean isRunning(){
+		return t!=null;
+	}
+}
